368a68bc311e61281c0997c7309bd272fef1e3fd
Viet Vo <viet.allen.qvo@gmail.com>
2020-08-10 16:29:57 -0700

    Completed demo of xor for integer variable switch

--- etc/xor.js
+++ etc/xor.js
@@ -13,7 +13,23 @@ let a = 5;
 let b = 10;
 
 // Logging the values of a and b
-console.log("This is the value of a: " + a);
-console.log("This is the value of b: " + b);
+console.log("This is the value of a: " + a); // Expected value: 5
+console.log("This is the value of b: " + b); // Expected value: 10
+
+// The xor operator in javascript is designated as "^" in javascript
+a = a ^ b;
+b = a ^ b;
+
+// Testing the new value of a and b
+console.log(a);
+console.log(b);
+
+// Although b is swapped, a still needs to be swapped with the value of b
+a = a ^ b;
+
+// Confirming the values are both switched
+console.log("This is the value of a: " + a); // Expected value: 10
+console.log("This is the value of b: " + b); // Expected value: 5
+
 
 
