1075ce294e5f975b05dd79a89896ef4f2a5e147d
Viet Vo <viet.allen.qvo@gmail.com>
2020-08-09 23:27:26 -0700

    Added notes and initialized for the xor and quicksort exercises

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -1,1 +1,13 @@
+/*
+This is an exercise of the quicksort algorithm
+
+? Learn the quicksort algorithm for organization of data
+? Learn pros and cons of quicksort
+? Learn use cases and when it should be avoided
+
+Resources:
+https://humanwhocodes.com/blog/2012/11/27/computer-science-in-javascript-quicksort/
+
+*/
+
 console.log("test")
---
+++ etc/xor.js
@@ -0,0 +1,19 @@
+/*
+The purpose of this exercise is to test the xor bitwise variable swap
+
+? Learning how and why xor can be used to switch values of variables can be used for sorting values
+
+Resources:
+https://stackoverflow.com/questions/249423/how-does-xor-variable-swapping-work
+
+*/
+
+// Initialized original variable values
+let a = 5; 
+let b = 10;
+
+// Logging the values of a and b
+console.log("This is the value of a: " + a);
+console.log("This is the value of b: " + b);
+
+
